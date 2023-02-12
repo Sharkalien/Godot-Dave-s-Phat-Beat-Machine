f647dc86095d109640cdddd4766b577483b7755c
Leo Esc <leonardodanielescoto@gmail.com>
2023-02-12 16:42:50 -0600

    Whatever, nothing important

--- BeatButton.gd
+++ BeatButton.gd
@@ -1,12 +1,12 @@
 extends TextureButton
 
-onready var audPlayer = $AudioStreamPlayer
+onready var audPlayer:AudioStreamPlayer = $AudioStreamPlayer
 
 
 func _on_BeatButton_toggled(button_pressed: bool) -> void:
 	if button_pressed:
-		audPlayer.playing = true
+		audPlayer.play()
 		self_modulate = Color(0.78, 0.99, 0.45, 1.00)
 	else:
-		audPlayer.playing = false
+		audPlayer.stop()
 		self_modulate = Color(1.00, 1.00, 1.00, 1.00)
--- JokeButton.gd
+++ JokeButton.gd
@@ -1,10 +1,10 @@
 extends Button
 
-onready var audPlayer = $AudioStreamPlayer
+onready var audPlayer:AudioStreamPlayer = $AudioStreamPlayer
 
 
 func _on_JokeButton_toggled(button_pressed: bool) -> void:
 	if button_pressed:
-		audPlayer.playing = true
+		audPlayer.play()
 	else:
-		audPlayer.playing = false
+		audPlayer.stop()
