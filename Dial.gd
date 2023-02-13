57c5d4956fa5268d687931b87cbbac357699a40e
Leo Esc <leonardodanielescoto@gmail.com>
2023-02-13 13:45:24 -0600

    Fix volume dials
    Dial variables were updating a frame too late.
    
    Also rotation is inverted in Godot compared to Flash, so just invert the dials' horizontal scale. BIG UPS TO RADICAL DUDE 42 FOR THINKING OF THIS, TYSM BRO

--- Dial.gd
+++ Dial.gd
@@ -4,6 +4,7 @@ onready var spin = $spin
 var rot = 0
 var rott = 0
 var rDiff = 180 / PI
+var isDown = false
 
 
 func _mouseMove() -> void:
@@ -18,15 +19,19 @@ func _mouseMove() -> void:
 	if rotDegrees > 0 && spin.rect_rotation < -90:
 		rotDegrees = -179.9
 	spin.rect_rotation = rotDegrees
-	print(rott)
+	print(rotDegrees)
 
 
 func _on_gui_input(_event: InputEvent) -> void:
-	if Input.is_action_pressed("click"):
+	if Input.is_action_pressed("click") and isDown:
 		_mouseMove()
 
 
 func _on_button_down() -> void:
+	isDown = true
 	rot = spin.rect_rotation
 	rott = atan2(get_global_mouse_position().y - position.y, get_global_mouse_position().x - position.x) * rDiff
-#	print(rott)
+
+
+func _on_button_up() -> void:
+	isDown = false
